2462b002d0eb382265b6bfedbb2aafebc8aebcc7
SzymonM06 <szymonmatlosz64@gmail.com>
2024-05-18 22:46:05 +0200

    Commit

--- TheList/EntryPanel.Designer.cs
+++ TheList/EntryPanel.Designer.cs
@@ -60,23 +60,24 @@
             this.entryPanel_.Controls.Add(this.textBox_Title);
             this.entryPanel_.Location = new System.Drawing.Point(0, 0);
             this.entryPanel_.Name = "entryPanel_";
-            this.entryPanel_.Size = new System.Drawing.Size(540, 338);
+            this.entryPanel_.Size = new System.Drawing.Size(540, 163);
             this.entryPanel_.TabIndex = 0;
             // 
             // pictureBox_Image
             // 
+            this.pictureBox_Image.BackColor = System.Drawing.Color.Gainsboro;
             this.pictureBox_Image.ImageLocation = "";
             this.pictureBox_Image.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox_Image.InitialImage")));
-            this.pictureBox_Image.Location = new System.Drawing.Point(6, 57);
+            this.pictureBox_Image.Location = new System.Drawing.Point(6, 5);
             this.pictureBox_Image.Name = "pictureBox_Image";
-            this.pictureBox_Image.Size = new System.Drawing.Size(229, 245);
+            this.pictureBox_Image.Size = new System.Drawing.Size(121, 127);
             this.pictureBox_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_Image.TabIndex = 9;
             this.pictureBox_Image.TabStop = false;
             // 
             // button_SelectImage
             // 
-            this.button_SelectImage.Location = new System.Drawing.Point(6, 308);
+            this.button_SelectImage.Location = new System.Drawing.Point(6, 138);
             this.button_SelectImage.Name = "button_SelectImage";
             this.button_SelectImage.Size = new System.Drawing.Size(93, 23);
             this.button_SelectImage.TabIndex = 8;
@@ -88,7 +89,7 @@
             // 
             this.moveUp.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.moveUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.moveUp.Location = new System.Drawing.Point(510, 275);
+            this.moveUp.Location = new System.Drawing.Point(510, 8);
             this.moveUp.Name = "moveUp";
             this.moveUp.Size = new System.Drawing.Size(27, 27);
             this.moveUp.TabIndex = 7;
@@ -100,7 +101,7 @@
             // 
             this.moveDown.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.moveDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.moveDown.Location = new System.Drawing.Point(510, 308);
+            this.moveDown.Location = new System.Drawing.Point(510, 50);
             this.moveDown.Name = "moveDown";
             this.moveDown.Size = new System.Drawing.Size(27, 27);
             this.moveDown.TabIndex = 6;
@@ -111,7 +112,7 @@
             // label_epCounter
             // 
             this.label_epCounter.AutoSize = true;
-            this.label_epCounter.Location = new System.Drawing.Point(58, 35);
+            this.label_epCounter.Location = new System.Drawing.Point(185, 35);
             this.label_epCounter.Name = "label_epCounter";
             this.label_epCounter.Size = new System.Drawing.Size(57, 13);
             this.label_epCounter.TabIndex = 5;
@@ -119,7 +120,7 @@
             // 
             // button_epMinus
             // 
-            this.button_epMinus.Location = new System.Drawing.Point(32, 31);
+            this.button_epMinus.Location = new System.Drawing.Point(159, 31);
             this.button_epMinus.Name = "button_epMinus";
             this.button_epMinus.Size = new System.Drawing.Size(20, 20);
             this.button_epMinus.TabIndex = 4;
@@ -129,7 +130,7 @@
             // 
             // button_epPlus
             // 
-            this.button_epPlus.Location = new System.Drawing.Point(6, 31);
+            this.button_epPlus.Location = new System.Drawing.Point(133, 31);
             this.button_epPlus.Name = "button_epPlus";
             this.button_epPlus.Size = new System.Drawing.Size(20, 20);
             this.button_epPlus.TabIndex = 3;
@@ -139,8 +140,9 @@
             // 
             // delete_checkBox
             // 
+            this.delete_checkBox.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.delete_checkBox.AutoSize = true;
-            this.delete_checkBox.Location = new System.Drawing.Point(241, 8);
+            this.delete_checkBox.Location = new System.Drawing.Point(480, 142);
             this.delete_checkBox.Name = "delete_checkBox";
             this.delete_checkBox.Size = new System.Drawing.Size(57, 17);
             this.delete_checkBox.TabIndex = 2;
@@ -151,7 +153,7 @@
             // label_Title
             // 
             this.label_Title.AutoSize = true;
-            this.label_Title.Location = new System.Drawing.Point(3, 8);
+            this.label_Title.Location = new System.Drawing.Point(133, 11);
             this.label_Title.Name = "label_Title";
             this.label_Title.Size = new System.Drawing.Size(30, 13);
             this.label_Title.TabIndex = 1;
@@ -159,7 +161,7 @@
             // 
             // textBox_Title
             // 
-            this.textBox_Title.Location = new System.Drawing.Point(35, 5);
+            this.textBox_Title.Location = new System.Drawing.Point(164, 8);
             this.textBox_Title.Name = "textBox_Title";
             this.textBox_Title.Size = new System.Drawing.Size(200, 20);
             this.textBox_Title.TabIndex = 0;
@@ -170,7 +172,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.entryPanel_);
             this.Name = "EntryPanel";
-            this.Size = new System.Drawing.Size(540, 338);
+            this.Size = new System.Drawing.Size(540, 163);
             this.entryPanel_.ResumeLayout(false);
             this.entryPanel_.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Image)).EndInit();
